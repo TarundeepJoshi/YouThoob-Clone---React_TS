8af355a3c86a9b1f0ccd27bebecb5ca8c03e4b2e
Tarundeep Joshi <tarundeepjoshi0002@gmail.com>
2024-10-19 14:29:10 +0530

    Videos data fetched from v3 api successfully

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useEffect } from "react";
+import { getHomePageVideos } from "../store/reducers/getHomePageVideos";
+
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state)=> state.youtubeApp.videos);
+
+  useEffect(() => {
+      dispatch(getHomePageVideos(false));
+  }, [dispatch]);
+  
+
   return (
     <div className="max-h-screen overflow-hidden">
       <div style={{ height: "7.5vh" }}>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { InitialState } from "../../Types";
+import { getHomePageVideos } from "./reducers/getHomePageVideos";
 
 const initialState: InitialState = {
   videos: [],
@@ -14,7 +15,12 @@ const youtubeSlice = createSlice({
   name: "youtubeApp",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(getHomePageVideos.fulfilled, (state, action)=>{
+      // state.videos = action.payload.parsedData;
+      // state.nextPageToken = action.payload.nextPageToken;
+    })
+  },
 });
 
 export const store = configureStore({
